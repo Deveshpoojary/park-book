18163e7a29b658de96069d3f366c25e3b66f3af5
Deveshpoojary <deveshpoojary05@gmail.com>
2024-05-26 22:51:05 +0530

    frontend design 6 responsive

--- src/pages/book.js
+++ src/pages/book.js
@@ -227,124 +227,130 @@ const Book = () => {
   return (
     <>
       {!confirm ?
-        <div className="bg-primary min-h-screen bg-neutral-900 text-white py-4 px-14 ">
-          <div className="container mx-auto py-8">
-            <div className="bg-neutral-800 p-8 rounded-2xl shadow-xl shadow-gray-500">
-              <h1 className="text-3xl font-bold mb-6">Parking Booking</h1>
-              <form id='bookingform'>
-                <div className="mb-4">
-                  <label htmlFor="vehicleType" className="font-medium">Vehicle Type:</label>
-                  <div className="flex gap-4 mt-2">
-                    <button
-                      type="button"
-                      onClick={() => handleVehicleTypeChange("car")}
-                      className={`py-2 px-4 rounded-md border border-gray-400 transition-colors ${booking.vehicleType === "car" ? "bg-cyan-500" : "bg-primary hover:bg-gray-600"
-                        } font-bold`}
-                    >
-                      <IoCarSportSharp size={30} className="text-black-500" />
-                      Car
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => handleVehicleTypeChange("bike")}
-                      className={`py-2 px-4 rounded-md border border-gray-400 transition-colors ${booking.vehicleType === "bike" ? "bg-cyan-500" : "bg-primary hover:bg-gray-600"
-                        } font-bold`}
-                    >
-                      <PiMotorcycleFill size={30} className="text-black-500" />
-                      Bike
-                    </button>
-                  </div>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                  <div>
-                    <label htmlFor="date" className="font-medium">Start Date:</label>
-                    <input type="date" name="date" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.date} />
-                  </div>
-                  <div>
-                    <label htmlFor="time" className="font-medium">Start Time:</label>
-                    <input type="time" name="time" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.time} />
-                  </div>
-                  <div>
-                    <label htmlFor="date2" className="font-medium">End Date:</label>
-                    <input type="date" name="date2" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.date2} />
-                  </div>
-                  <div>
-                    <label htmlFor="endTime" className="font-medium">End Time:</label>
-                    <input type="time" name="endTime" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.endTime} />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <label htmlFor="vehicleNumber" className="font-medium">Vehicle Number:</label>
-                  <input type="text" name="vehicleNumber" placeholder="KA-01-AB-1234" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary" onChange={handleChange} value={booking.vehicleNumber} />
-                </div>
-                {error && <div className="bg-red-700 text-center p-3 rounded mb-4">
-                  {error}
-                </div>}
-                {/* Total Charges Display */}
-                {amount > 0 ? <p>Total Charges:<span className='font-medium text-cyan-500'>INR: {amount.toFixed(2)}</span> </p> : <span className="relative flex h-3 w-3">Fetching
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                </span>}
-                {/* Slot Selection */}
-                <div className="mb-6 mt-2">
-                  <label className="block text-sm font-medium text-gray-700">Select Slot</label>
-                  {!loading? ( <div className="grid grid-cols-4 gap-4 mt-2">
-                    {currentSlots.map(slot => (
-                      <div key={slot.slotId} onClick={() => handleSlotSelect(slot.slotId)}
-                        className={`cursor-pointer p-3 ${slot.slotId === booking.slotId ? 'bg-cyan-400 text-black' : slot.isOccupied ? 'bg-red-300 text-white' : 'bg-slate-500 text-white'} rounded-lg`}>
-                        Slot {slot.slotId}
-                      </div>
-                    ))}
-                  </div> ) : (<LoadingAnimation/>)}
-                  {slots.length > 0 && (
-                    <div className="flex justify-between mt-4">
-                      <button disabled={currentPage === 1} onClick={handlePreviousPage} className="bg-gray-400 text-black font-bold py-2 px-4 rounded disabled:opacity-50">
-                        Previous
-                      </button>
-                      <button disabled={currentSlots.length < slotsPerPage} onClick={handleNextPage} className="bg-gray-400 text-black font-bold py-2 px-4 rounded disabled:opacity-50">
-                        Next
-                      </button>
-                    </div>
-                  )}
-                  {error && <Alert variant="filled" severity="error" onClose={() => { setError(null) }}>{error}</Alert>}
-                </div>
-                {/* Booking Button */}
-                <button type="submit" className="w-full bg-white text-black font-bold py-2 px-4 rounded-md hover:bg-black hover:text-white border border-gray-400 flex justify-center items-center" onClick={handleBooking}>
-                  Book Now <FaRegArrowAltCircleRight size={25} className='ml-1' />
+      <div className="bg-primary min-h-screen bg-neutral-900 text-white py-4 px-4 sm:px-2 md:px-6 lg:px-10">
+      <div className="container mx-auto py-8">
+        <div className="bg-neutral-800 p-4 sm:p-6 lg:p-8 rounded-2xl shadow-xl shadow-gray-500">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">Parking Booking</h1>
+          <form id="bookingform">
+            <div className="mb-4">
+              <label htmlFor="vehicleType" className="font-medium">Vehicle Type:</label>
+              <div className="flex gap-4 mt-2">
+                <button
+                  type="button"
+                  onClick={() => handleVehicleTypeChange("car")}
+                  className={`py-2 px-4 rounded-md border border-gray-400 transition-colors ${booking.vehicleType === "car" ? "bg-cyan-500" : "bg-primary hover:bg-gray-600"} font-bold`}
+                >
+                  <IoCarSportSharp size={30} className="text-black-500" />
+                  Car
                 </button>
-              </form>
-            </div>
-          </div>
-          {/* Confirmation Modal */}
-          {isConfirmModalVisible && (
-            <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center p-4">
-              <div className="bg-slate-700 rounded-lg p-6 max-w-sm mx-auto">
-                <h2 className="text-lg font-semibold">Confirm Booking</h2>
-                <p className="text-sm">Please confirm your booking details:</p>
-                <ul className="text-sm list-disc pl-5 mt-2">
-                  <li>Vehicle Type: {booking.vehicleType}</li>
-                  <li>Vehicle Number: {booking.vehicleNumber}</li>
-                  <li>Start Date: {booking.date}</li>
-                  <li>Start Time: {booking.time}</li>
-                  <li>End Date: {booking.date2}</li>
-                  <li>End Time: {booking.endTime}</li>
-                  <li>Slot: {booking.slotId}</li>
-                  <li>Amount: INR: {amount.toFixed(2)}</li>
-                </ul>
-                <div className="flex justify-end gap-4 mt-4">
-                  <button onClick={() => setIsConfirmModalVisible(false)} className="bg-red-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded flex justify-center items-center">
-                    Cancel<MdCancel className="ml-2" />
-                  </button>
-                  {!loading ? <button onClick={confirmBooking} className="bg-cyan-500 hover:bg-sky-600 text-black font-bold py-2 px-4 rounded flex justify-center items-center">
-                    Pay and Confirm<GiConfirmed className="ml-2" />
-                  </button> : <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
-                    <BsTicketPerforated className="animate-spin" />
-                  </button>}
-                </div>
+                <button
+                  type="button"
+                  onClick={() => handleVehicleTypeChange("bike")}
+                  className={`py-2 px-4 rounded-md border border-gray-400 transition-colors ${booking.vehicleType === "bike" ? "bg-cyan-500" : "bg-primary hover:bg-gray-600"} font-bold`}
+                >
+                  <PiMotorcycleFill size={30} className="text-black-500" />
+                  Bike
+                </button>
               </div>
             </div>
-          )}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+              <div>
+                <label htmlFor="date" className="font-medium">Start Date:</label>
+                <input type="date" name="date" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.date} />
+              </div>
+              <div>
+                <label htmlFor="time" className="font-medium">Start Time:</label>
+                <input type="time" name="time" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.time} />
+              </div>
+              <div>
+                <label htmlFor="date2" className="font-medium">End Date:</label>
+                <input type="date" name="date2" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.date2} />
+              </div>
+              <div>
+                <label htmlFor="endTime" className="font-medium">End Time:</label>
+                <input type="time" name="endTime" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary picker" onChange={handleChange} value={booking.endTime} />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="vehicleNumber" className="font-medium">Vehicle Number:</label>
+              <input type="text" name="vehicleNumber" placeholder="KA-01-AB-1234" required className="form-input w-full mt-1 p-2 bg-gray-700 border border-gray-400 rounded bg-primary" onChange={handleChange} value={booking.vehicleNumber} />
+            </div>
+            {error && <div className="bg-red-700 text-center p-3 rounded mb-4">
+              {error}
+            </div>}
+            {/* Total Charges Display */}
+            {amount > 0 ? <p>Total Charges:<span className='font-medium text-cyan-500'>INR: {amount.toFixed(2)}</span> </p> : <span className="relative flex h-3 w-3">Fetching
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            </span>}
+            {/* Slot Selection */}
+            <div className="mb-6 mt-2">
+              <label className="block text-sm font-medium text-gray-700">Select Slot</label>
+              {!loading ? (
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-2">
+                  {currentSlots.map(slot => (
+                    <div key={slot.slotId} onClick={() => handleSlotSelect(slot.slotId)}
+                      className={`cursor-pointer p-3 ${slot.slotId === booking.slotId ? 'bg-cyan-400 text-black' : slot.isOccupied ? 'bg-red-300 text-white' : 'bg-slate-500 text-white'} rounded-lg`}>
+                      Slot {slot.slotId}
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <LoadingAnimation />
+              )}
+              {slots.length > 0 && (
+                <div className="flex justify-between mt-4">
+                  <button disabled={currentPage === 1} onClick={handlePreviousPage} className="bg-gray-400 text-black font-bold py-2 px-4 rounded disabled:opacity-50">
+                    Previous
+                  </button>
+                  <button disabled={currentSlots.length < slotsPerPage} onClick={handleNextPage} className="bg-gray-400 text-black font-bold py-2 px-4 rounded disabled:opacity-50">
+                    Next
+                  </button>
+                </div>
+              )}
+              {error && <Alert variant="filled" severity="error" onClose={() => { setError(null) }}>{error}</Alert>}
+            </div>
+            {/* Booking Button */}
+            <button type="submit" className="w-full bg-white text-black font-bold py-2 px-4 rounded-md hover:bg-black hover:text-white border border-gray-400 flex justify-center items-center" onClick={handleBooking}>
+              Book Now <FaRegArrowAltCircleRight size={25} className='ml-1' />
+            </button>
+          </form>
         </div>
+      </div>
+      {/* Confirmation Modal */}
+      {isConfirmModalVisible && (
+        <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center p-4">
+          <div className="bg-slate-700 rounded-lg p-6 max-w-sm mx-auto">
+            <h2 className="text-lg font-semibold">Confirm Booking</h2>
+            <p className="text-sm">Please confirm your booking details:</p>
+            <ul className="text-sm list-disc pl-5 mt-2">
+              <li>Vehicle Type: {booking.vehicleType}</li>
+              <li>Vehicle Number: {booking.vehicleNumber}</li>
+              <li>Start Date: {booking.date}</li>
+              <li>Start Time: {booking.time}</li>
+              <li>End Date: {booking.date2}</li>
+              <li>End Time: {booking.endTime}</li>
+              <li>Slot: {booking.slotId}</li>
+              <li>Amount: INR: {amount.toFixed(2)}</li>
+            </ul>
+            <div className="flex justify-end gap-4 mt-4">
+              <button onClick={() => setIsConfirmModalVisible(false)} className="bg-red-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded flex justify-center items-center">
+                Cancel<MdCancel className="ml-2" />
+              </button>
+              {!loading ? (
+                <button onClick={confirmBooking} className="bg-cyan-500 hover:bg-sky-600 text-black font-bold py-2 px-4 rounded flex justify-center items-center">
+                  Pay and Confirm<GiConfirmed className="ml-2" />
+                </button>
+              ) : (
+                <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+                  <BsTicketPerforated className="animate-spin" />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
         :
         <ConfirmationPage />}
     </>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,92 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth0, } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import Profile from './Profile';
-import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaRegArrowAltCircleRight, FaBars, FaTimes } from "react-icons/fa";
 import { GiFallingStar } from "react-icons/gi";
 import { IoCarSportSharp } from "react-icons/io5";
 
-//import { FaBolt } from "react-icons/fa";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth0();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-    const navigate = useNavigate();
-    const { logout } = useAuth0();
-    const handleClick = (event) => {
-        event.preventDefault();
-        navigate('/book');
+  const handleNavigation = (path) => {
+    navigate(path);
+    setIsNavOpen(false);
+  };
 
+  return (
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 bg-primary">
+      <Profile />
 
-
-    };
-    return (
-
-
-
-
-
-
-
-        <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 bg-primary  ">
-            <Profile />
-
-            <div className='bg-secondary flex items-center justify-between px-4 py-2 shadow-lg '>
-                <div>
-                    <img src="https://api.time.com/wp-content/uploads/2015/02/apple-logo.jpg" alt="Logo" className="h-8 w-auto" />
-                </div>
-                <div>
-                    <button onClick={() => { navigate('/book'); }} className="bg-white hover:bg-black hover:text-white border border-white text-black font-bold py-2 px-4 rounded-2xl">
-                        Book
-                    </button>
-                    <button onClick={() => { navigate('/admin'); }} className="ml-4 bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">
-                        Admin
-                    </button>
-                    <button onClick={() => { navigate('/adminhist'); }} className="ml-4 bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">Admin history</button>
-                    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="ml-4 bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">Log Out </button>
-                </div>
-
-                <div>
-                    <button onClick={() => { navigate('/history'); }} className="ml-4 bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">History</button>
-                </div>
-            </div>
-            <div className="relative py-3 sm:max-w-xl sm:mx-auto"></div>
-
-            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-                <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-                <div className="bg-secondary border border-gray-400 relative px-4 py-10 bg-neutral-800 text-white shadow-lg sm:rounded-3xl sm:p-20 inset-0 ">
-                    <div className="max-w-md mx-auto">
-                        <div>
-                            {/* <img src="https://api.time.com/wp-content/uploads/2015/02/apple-logo.jpg" className="h-7 sm:h-8" alt="logo" /> */}
-                            <IoCarSportSharp size={50} color='cyan' className='margin'/>
-
-                        </div>
-                        <div className="divide-y divide-gray-200">
-                            <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                                <p className='text-white text-lg font-bold flex'>An advanced online parking booking system <GiFallingStar size={30} color='cyan' className='ml-1' /></p>
-                                <form className="mt-6" >
-                                    <div>
-                                        <label htmlFor="location" className="sr-only">Location</label>
-
-                                        <select className='bg-gray-700 rounded text-white'>
-                                            <option value="location">Railway MlR CNTRL</option>
-                                            <option value="location">none</option>
-                                        </select>
-                                    </div>
-                                    <div className="mt-6">
-                                        <button onClick={handleClick} className="w-full mr-auto px-4 py-2 text-black font-bold bg-slate-100 hover:bg-neutral-900 hover:text-white rounded-md focus:outline-none flex justify-center items-center border border-white">  Book a parking space <FaRegArrowAltCircleRight size={25} className='ml-1' /></button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      {/* Navigation bar */}
+      <div className="bg-secondary flex items-center justify-between px-4 py-2 shadow-lg">
+        <div>
+          <img src="https://api.time.com/wp-content/uploads/2015/02/apple-logo.jpg" alt="Logo" className="h-8 w-auto" />
+            {/* <h1 className='text-white font-bold text-3xl'>ParkWay</h1> */}
         </div>
+        {/* Navbar for medium and small screens */}
+        <div className="md:hidden">
+          <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-white">
+            <FaBars size={24} />
+          </button>
+        </div>
+        {/* Navbar for large screens */}
+        <div className="hidden md:flex space-x-4">
+          <button onClick={() => handleNavigation('/book')} className="bg-white hover:bg-black hover:text-white border border-white text-black font-bold py-2 px-4 rounded-2xl">Book</button>
+          <button onClick={() => handleNavigation('/admin')} className="bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">Admin</button>
+          <button onClick={() => handleNavigation('/adminhist')} className="bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">Admin history</button>
+          <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">Log Out</button>
+          <button onClick={() => handleNavigation('/history')} className="bg-white hover:bg-black hover:text-white border border-white font-bold py-2 px-4 rounded-2xl">History</button>
+        </div>
+      </div>
 
-    );
+      {/* Slide-in navbar for medium and small screens */}
+      <div className={`fixed inset-0 z-50 flex transition-transform transform ${isNavOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className="flex-1" onClick={() => setIsNavOpen(false)} />
+        <div className="bg-secondary flex flex-col w-64 h-full p-4">
+          <div className="flex justify-end">
+            <button onClick={() => setIsNavOpen(false)} className="text-white">
+              <FaTimes size={24} />
+            </button>
+          </div>
+          <button onClick={() => handleNavigation('/book')} className="mt-4 text-white font-bold py-2 px-4 hover:bg-white hover:text-black hover:rounded-md">Book</button>
+          <button onClick={() => handleNavigation('/admin')} className="mt-2 text-white font-bold py-2 px-4 hover:bg-white hover:text-black hover:rounded-md">Admin</button>
+          <button onClick={() => handleNavigation('/adminhist')} className="mt-2 text-white font-bold py-2 px-4 hover:bg-white hover:text-black hover:rounded-md">Admin history</button>
+          <button onClick={() => handleNavigation('/history')} className="mt-2 text-white font-bold py-2 px-4 hover:bg-white hover:text-black hover:rounded-md">History</button>
+          <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="mt-2 text-white font-bold py-2 px-4 hover:bg-white hover:text-black hover:rounded-md">Log Out</button>
+        </div>
+      </div>
+
+      {/* Main content */}
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto px-4"></div>
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto sm:px-0 px-2">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
+        <div className="bg-secondary border border-gray-400 relative px-4 py-10 bg-neutral-800 text-white shadow-lg rounded-3xl sm:p-20 inset-0 ">
+          <div className="max-w-md mx-auto">
+            <div>
+              <IoCarSportSharp size={50} color='cyan' className='margin' />
+            </div>
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p className='text-white text-lg font-bold flex'>An advanced online parking booking system <GiFallingStar size={30} color='cyan' className='ml-1' /></p>
+                <form className="mt-6">
+                  <div>
+                    <label htmlFor="location" className="sr-only">Location</label>
+                    <select className='bg-gray-700 rounded text-white'>
+                      <option value="location">Railway MlR CNTRL</option>
+                      <option value="location">none</option>
+                    </select>
+                  </div>
+                  <div className="mt-6">
+                    <button onClick={() => handleNavigation('/book')} className="w-full mr-auto px-4 py-2 text-black font-bold bg-slate-100 hover:bg-neutral-900 hover:text-white rounded-md focus:outline-none flex justify-center items-center border border-white">  Book a parking space <FaRegArrowAltCircleRight size={25} className='ml-1' /></button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withAuthenticationRequired(HomePage, {
-    // Show a message while the user waits to be redirected to the login page.
-    onRedirecting: () => (<div>Redirecting you to the login page...</div>)
+  // Show a message while the user waits to be redirected to the login page.
+  onRedirecting: () => (<div>Redirecting you to the login page...</div>)
 });
